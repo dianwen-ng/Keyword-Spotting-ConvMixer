5cc3df980760bef7398a83b5bf939b5542d0802d
Dianwen <35693995+dianwen-ng@users.noreply.github.com>
2022-08-10 16:14:23 +0800

    Update main.py
    fix for updating acc metric

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def parse_args():
     args = parser.parse_args()
     
     with open(args.config, "r") as file:
-        conf = yaml.load(file)
+        conf = yaml.full_load(file)
 
     return conf
 
@@ -39,10 +39,9 @@ def parse_args():
 def train(model, device, train_loader, optimizer, scaler, epoch, log_interval=100):
     ## set training mode
     model.train()  
-    iteration = 0
     train_loss = 0.
-    start = time()
     gt_labels, pred_labels = [], []
+    start = time()
     
     criterion = nn.BCEWithLogitsLoss().to(device)
     
@@ -55,36 +54,32 @@ def train(model, device, train_loader, optimizer, scaler, epoch, log_interval=10
             output, _ = model(data)
             loss = criterion(output, target)
         
-        train_loss += loss
+        train_loss += loss.item()
         optimizer.zero_grad()
         scaler.scale(loss).backward()
         scaler.step(optimizer)
         scaler.update()
         
-        pred = output.cpu().data.numpy().argmax(axis=1)
-        target_lab = target.cpu().data.numpy().argmax(axis=1)
-        correct += int((pred == target_lab).sum())
-        acc = 100. * correct / len(target)
+        pred = output.detach().cpu().numpy().argmax(axis=1)
+        gt_labels += target.detach().cpu().numpy().argmax(axis=1).tolist()
+        pred_labels += pred.tolist()
+        up_to_date_acc = (np.array(pred_labels) == np.array(gt_labels)).sum()
+        up_to_date_acc = (up_to_date_acc / len(pred_labels)) * 100.
         
-        if iteration % log_interval == 0:
+        if batch_idx % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\t Loss: {:.5f} Acc: {:.3f}%'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                    100 * batch_idx / len(train_loader), loss.item(), acc))
-    
-        gt_labels.append(target_lab)
-        pred_labels.append(pred)
-        
-        iteration += 1
+                  epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100 * batch_idx / len(train_loader), loss.item(), up_to_date_acc))
     
     end = time()
     train_loss /= len(train_loader.dataset)
+    acc = (np.array(pred_labels) == np.array(gt_labels)).sum() / len(pred_labels)
     print('Train loss is {:.5f} after Epoch {}'.format(train_loss, epoch))
-    print('Total accuracy is {:.3f} after Epoch {}'.format(
-        accuracy_score(np.hstack(gt_labels), np.hstack(pred_labels)), epoch))
+    print('Total accuracy is {:.3f} after Epoch {}'.format(acc, epoch))
     print('Time taken for Epoch {} is {:.4f}s'.format(epoch, end - start))
 
     
-def test(model, device, test_loader, ep):
+def test(model, device, test_loader, epoch):
     
     print('Sample size of validation set: ' + str(len(test_loader.dataset)))
     model.eval()
@@ -102,21 +97,18 @@ def test(model, device, test_loader, ep):
 
             output, _ = model(data)
             loss = criterion(output, target) 
-            test_loss += loss
-            pred = output.cpu().data.numpy().argmax(axis=1)
-            target_lab = target.cpu().data.numpy().argmax(axis=1)
-            correct += int((pred == target_lab).sum())
+            test_loss += loss.item()
             
-            gt_labels.append(target_lab)
-            pred_labels.append(pred)
-    
+            pred = output.detach().cpu().numpy().argmax(axis=1)
+            pred_labels += pred.tolist()
+            gt_labels += target.detach().cpu().numpy().argmax(axis=1).tolist()
+            
     end = time()
     test_loss /= len(test_loader.dataset)
-    
-    test_acc = accuracy_score(np.hstack(gt_labels), np.hstack(pred_labels))
-    print('Valid loss is {:.5f} after Epoch {}'.format(test_loss, ep))
-    print('Valid accuracy is {:.3f} after Epoch {}'.format(test_acc, ep))
-    
+    test_acc = (np.array(pred_labels) == np.array(gt_labels)).sum() / len(pred_labels)
+    print('Valid loss is {:.5f} after Epoch {}'.format(test_loss, epoch))
+    print('Valid accuracy is {:.3f} after Epoch {}'.format(test_acc, epoch))
+    print('Time taken for testing: {:.4f}s'.format(end - start))
     return round(test_acc, 3)
 
 
@@ -129,15 +121,15 @@ def setup_seed(seed):
 
 
 def main():
-    
     args = parse_args()
     setup_seed(args.get('seed'))
-    use_cuda = torch.cuda.is_available() and args.get('use_gpu')
+    use_cuda = torch.cuda.is_available() 
     device = torch.device("cuda" if use_cuda else "cpu")
     print("Training using: ", device)
     
     ## make model
-    model = KWSConvMixer(input_size = args.get('model')["input_size"], 
+    input_size = (int(i) for i in args.get('model')["input_size"].split(','))
+    model = KWSConvMixer(input_size = input_size, 
                          num_classes=args.get('model')["num_classes"],
                          feat_dim=args.get('model')["feat_dim"],
                          dropout=args.get('model')["dropout"]).to(device)
@@ -171,6 +163,11 @@ def main():
     
     print('Starting model training ...')
     for epoch in range(args.get("trainer")["num_epochs"]):
+        
+        ## turning off mixup at epoch 85 for post stage finetuning
+        if epoch == 85:
+            train_loader.dataset.mixup = 0
+            
         print('\n=============start training=============')
         train(model, device, train_loader, optimizer, scaler, epoch, args.get("trainer")["log_interval"])
         
@@ -185,4 +182,6 @@ def main():
         torch.save(state_dict, model_save_path)
     
 if __name__ == '__main__':
+    os.environ['CUDA_VISIBLE_DEVICES']="2"
     main()
+
